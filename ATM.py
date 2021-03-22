54e9cacf7268246e87ac2f6fdba3794860aea40b
EL001 <59197738+EL001@users.noreply.github.com>
2021-03-22 16:26:33 +0100

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,24 @@
+class ATM:
+    def __init__(self, cash = 1000):
+        self.cash = cash
+        print ("Account balance is", cash)
+    
+    def addcash(self, cash_add):
+        self.cash_add = cash_add
+        self.cash = cash_add + self.cash
+        print ("Total cash is", self.cash)
+           
+    def withdraw(self):
+        withdrawal_amt = int(input("cash to withdraw "))
+        self.cash = self.cash - withdrawal_amt
+        print ("Total cash after withdrawal is ", self.cash)
+        
+    def balance(self):
+        print ("Total amount in account is ", self.cash)
+    
+money = ATM()
+money.addcash(100)
+
+money.withdraw() 
+
+money.balance()
